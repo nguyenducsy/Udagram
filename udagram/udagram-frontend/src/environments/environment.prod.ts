03156ed4d4ff7d9e1064fc943c7b84c97d4374ce
Sỹ Nguyễn Đức <nguyenducsy3577@gmail.com>
2024-08-11 22:04:54 +0700

    commit #1

---
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  appName: 'Udagram',
+  apiHost: 'http://myudagramapi-env.eba-nmmjmkts.us-east-1.elasticbeanstalk.com/api/v0',
+};
+
+
---
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  appName: 'Udagram',
+  apiHost: 'http://localhost:8080/api/v0'
+};
+
